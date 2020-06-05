447f038e444e9c80c98af1c61b780dfe35798ff6
tokiedokie <42903136+tokiedokie@users.noreply.github.com>
2020-06-05 17:52:49 +0900

    percent_decode works

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,15 @@ fn parse_uri(request: String) -> (String, String) {
 }
 
 fn percent_decode(input: &str) -> String {
-    String::new()
+    let byte_array: Vec<u8> = input
+        .split('%')
+        .filter(|str| !str.is_empty() )
+        .map(|s| u8::from_str_radix(s, 16).unwrap())
+        .collect();
+
+    let decoded = String::from_utf8(byte_array).unwrap();
+
+    decoded
 }
 
 fn handle_dir(path: &Path) -> Result<Vec<u8>, Box<dyn Error>> {
